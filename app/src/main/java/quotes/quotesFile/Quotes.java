42cc0f728cca618fac2dd5886e4e21b048b8d2d1
LayanaBaba <leana_baba@yahoo.com>
2021-07-06 18:54:07 +0300

    finished lab 08

---
+++ app/src/main/java/quotes/quotesFile/Quotes.java
@@ -0,0 +1,19 @@
+package quotes.quotesFile;
+
+public class Quotes {
+    private String text;
+    private String author;
+
+    public Quotes(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
